e4908aaf1bfb5ef4b14eb2b2760a5589e90a7856
Roman Morozov <ramorozov1984@gmail.com>
2024-05-14 20:38:49 +0200

    Created Category and Course models

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from django.utils import timezone
+
+#Создание модели для категорий курсов
+class Category(models.Model):
+    #Задаем поля БД и их параметры
+    #Заголовок категории, символы, 255 символов
+    title = models.CharField(max_length=255)
+    #Время создания категории, дата, текущая дата генерируется автоматически
+    created_at = models.DateTimeField(default=timezone.now)
+
+#Создание модели для курсов
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    #Цена число с плавающей точкой
+    price = models.FloatField()
+    #Кол-во студентов, целое число
+    students_qty = models.IntegerField()
+    #Количество отзывов
+    reviews_qty = models.IntegerField()
+    #Категории связаны с моделью Category, 
+    #будут удалятся все курсы при удалении категории
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
