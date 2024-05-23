5bbc1af4f3a2394b8e573d200dfc7acc4637960a
Roman Morozov <ramorozov1984@gmail.com>
2024-05-23 16:13:46 +0200

    Added views function for courses pages

--- shop/views.py
+++ shop/views.py
@@ -5,7 +5,7 @@ from .models import Course
 
 # Create your views here.
 """
-Функция index отвечает за то, что будет возващаться
+Функция index отвечает за то, что будет возвращаться
 при обращении на главную страницу приложения Shop
 """
 def index(request):
@@ -19,3 +19,13 @@ def index(request):
 
     #Вывод данных в шаблон courses
     return render(request, 'courses.html', {'courses': courses})
+
+"""
+Функция single_course отвечает за то, что будет возвращаться
+при обращении на страницу курса приложения Shop
+"""
+def single_course(request, course_id):
+    #Поиск курса по primary key
+    course = Course.objects.get(pk=course_id)
+    #Рендеринг шаблона single_course.html
+    return render(request, 'single_course.html', {'course': course})
