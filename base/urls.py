ba0cbfd312550a90e6d8498b2516cdf94dfe237b
Roman Morozov <ramorozov1984@gmail.com>
2024-07-02 20:04:36 +0200

    Created routing for app Api

--- base/urls.py
+++ base/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.models import CourseResource, CategoryResource
 
+#Создадим экземляры класса CourseResource, CategoryResource
+course_resource = CourseResource()
+category_resource = CategoryResource()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #Маршрутиризация к приложению Shop с включением всех маршрутов из shop.urls
-    path('shop/', include('shop.urls'))
+    path('shop/', include('shop.urls')),
+    #Маршрутизация приложения Api
+    path('api/', include(course_resource.urls)),
+    path('api/', include(category_resource.urls))
 ]
